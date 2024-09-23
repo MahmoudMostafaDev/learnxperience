a720fbd5c5903af822d700e720efc03e2eb9a6eb
kodakodama <163364396+KodaKodama@users.noreply.github.com>
2024-09-23 20:19:56 +0530

    feat: set up server and establish database connection

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './db/db.js';
+dotenv.config({
+    path:'./.env'
+})
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+app.use(cors());
+
+app.get('api/v1/users', (req, res)=> {
+    res.send("hello there");
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+})
