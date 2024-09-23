56a2c767bb9172b121754e22da283c449874a896
kodakodama <163364396+KodaKodama@users.noreply.github.com>
2024-09-23 21:47:33 +0530

    refactor: move database connection file to app/lib for better organization

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import connectDB from './db/db.js';
+import connectDB from '../app/lib/db.js';
 dotenv.config({
     path:'./.env'
 })
