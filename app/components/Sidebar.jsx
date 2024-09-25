06f93f58fb6a378357905d3881aa654c7342b95b
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-09-25 08:09:28 +0100

    error: Two same components are there while I commit that's causing error

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -31,7 +31,7 @@ export default function SideBar() {
                             {item.name}
                         </span>
                     ) : (
-                        ''
+                            ''
                     )}
                 </div>
             ))}
