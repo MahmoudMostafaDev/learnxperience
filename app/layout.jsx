0f2e5f2f4721f42e5fb3bf57be21e23ef85f9d90
Ridwan <ridwanyinusa15@gmail.com>
2024-10-06 00:22:09 +0100

    fix: fix font path

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,6 @@
-import localFont from 'next/font/local';
 import './globals.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata = {
   title: 'LearnXperience',
@@ -53,7 +41,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={` ${geistSans.variable} ${geistMono.variable}  antialiased bg-gray-50 text-gray-900 inter`}>
+      <body className={'bg-gray-50 text-gray-900 inter'}>
         <Navbar />
         {children}
         <Footer />
